f3e0cf72152da3cf5df075423dec1e15756bfd4f
Noor <nur.k@thearc.cc>
2025-06-29 01:39:48 +0600

    Fix HMR WebSocket Connection

--- vite.config.ts
+++ vite.config.ts
@@ -41,6 +41,12 @@ export default defineConfig({
     },
     host: "0.0.0.0",
     port: 5173,
+    hmr: {
+      host: 'localhost',
+      port: 5000,
+      clientPort: 5000,
+      protocol: 'wss'
+    },
     watch: {
       usePolling: true,
       interval: 1000,
